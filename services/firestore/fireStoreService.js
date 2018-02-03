4014b1780defe90dc4359f9cf11930b00d53d018
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2018-02-03 19:10:43 -0200

    Update fireStoreService.js

--- services/firestore/fireStoreService.js
+++ services/firestore/fireStoreService.js
@@ -1,3 +1,27 @@
+angular.module('listaTelefonica').value('configValues', {       
+        PageSize: 100,
+        apiBaseUrl: 'https://mauroao-lista-telefonica-api.herokuapp.com/api',
+        firestoreConfig: { 
+                apiKey: 'AIzaSyDOvpRFiUUnTNlJr2Nh9L1K0eWWiBP8lTc',
+                authDomain: 'projetofirestore.firebaseapp.com',
+                databaseURL: 'https://projetofirestore.firebaseio.com',
+                projectId: 'projetofirestore',
+                storageBucket: 'projetofirestore.appspot.com',
+                messagingSenderId: '322473565928'
+        }
+});
+
+/* 
+        url do servico interno do node js
+        apiBaseUrl: 'http://localhost:3000/api' 
+*/
+
+
+/* 
+        url do servico do node js na nuvem
+        apiBaseUrl: 'https://mauroao-lista-telefonica-api.herokuapp.com/api' 
+*/
+
 angular.module('listaTelefonica').service('fireStoreService', function($location, $rootScope, configValues){
 
 	$rootScope.api_version = configValues.firestoreConfig.databaseURL;
